6423e5db09713c93ae16d295b882e6768f029c69
Carlos Henrique <developerch@outlook.com>
2021-09-02 12:26:22 -0300

    Rest vehicle control updated : Added endpoint /vehicles and method findAll

--- backend/src/main/java/br/com/movements/wharehouse/resources/VehicleResource.java
+++ backend/src/main/java/br/com/movements/wharehouse/resources/VehicleResource.java
@@ -1,8 +1,27 @@
 package br.com.movements.wharehouse.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.movements.wharehouse.domain.Vehicle;
+import br.com.movements.wharehouse.services.VehicleService;
+
 @RestController
+@RequestMapping(value="/vehicles")
 public class VehicleResource {
+	@Autowired
+	private VehicleService service;
+	
+	@GetMapping
+	public ResponseEntity<?> findAll(@PageableDefault Pageable pageable){
+		Page<Vehicle> vehicles = service.findAll(pageable);
+		return ResponseEntity.ok(vehicles);
+	}
 
 }
