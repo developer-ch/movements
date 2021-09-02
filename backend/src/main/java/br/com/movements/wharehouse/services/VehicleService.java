7ac3a8a4cf2050cd50dfc6696ed7d8b1bf5303a8
Carlos Henrique <developerch@outlook.com>
2021-09-02 12:00:38 -0300

    Vehicle service updated : Including findAll and findOne methods

--- backend/src/main/java/br/com/movements/wharehouse/services/VehicleService.java
+++ backend/src/main/java/br/com/movements/wharehouse/services/VehicleService.java
@@ -1,8 +1,26 @@
 package br.com.movements.wharehouse.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import br.com.movements.wharehouse.domain.Vehicle;
+import br.com.movements.wharehouse.repositories.VehicleRepository;
 
 @Service
 public class VehicleService{
-
+	@Autowired
+	private VehicleRepository repository;
+	
+	public Page<Vehicle> findAll(Pageable pageable){
+		return repository.findAll(pageable);
+	}
+	
+	public Vehicle findOne(Long id) {
+		Optional<Vehicle> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
 }
