dc69319b4e3d61ff646f154e52bfad7337dd2af6
Carlos Henrique <developerch@outlook.com>
2021-09-02 09:58:23 -0300

    Vehicle entity created

---
+++ backend/src/main/java/br/com/movements/wharehouse/domain/Vehicle.java
@@ -0,0 +1,10 @@
+package br.com.movements.wharehouse.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Vehicle implements Serializable{
+	private static final long serialVersionUID = 1L;	
+}
