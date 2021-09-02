616c193cd3ce17741b4c220bf0d5d8373648a015
Carlos Henrique <developerch@outlook.com>
2021-09-02 09:54:52 -0300

    First commit

---
+++ backend/src/test/java/br/com/movements/MovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.movements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
