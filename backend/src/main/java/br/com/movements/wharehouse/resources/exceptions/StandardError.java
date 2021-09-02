1bae1822449d75775502f992945ea49d5531d226
Carlos Henrique <developerch@outlook.com>
2021-09-02 15:25:35 -0300

    Created helper class to trap exceptions

---
+++ backend/src/main/java/br/com/movements/wharehouse/resources/exceptions/StandardError.java
@@ -0,0 +1,19 @@
+package br.com.movements.wharehouse.resources.exceptions;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class StandardError implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer status;
+	private String message;
+	private Long timeStamp;
+	
+
+}
