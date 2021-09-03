bb3b481cc794e60fc7c908e66b884b22bbd265b5
Carlos Henrique <developerch@outlook.com>
2021-09-03 09:24:10 -0300

    Delete vehicle instantiation operation

--- backend/src/main/java/br/com/movements/MovementsApplication.java
+++ backend/src/main/java/br/com/movements/MovementsApplication.java
@@ -1,30 +1,13 @@
 package br.com.movements;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.movements.wharehouse.domain.Vehicle;
-import br.com.movements.wharehouse.repositories.VehicleRepository;
-
 @SpringBootApplication
-public class MovementsApplication implements CommandLineRunner{
-	@Autowired
-	private VehicleRepository repository;
+public class MovementsApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MovementsApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Vehicle vehicle = new Vehicle(null,"CCC9C99", "CCC0C00",true);
-		Vehicle vehicle2 = new Vehicle(null,"CCC9C97", "CCC0C00",true);
-		
-		repository.saveAll(Arrays.asList(vehicle,vehicle2));
 	}
 
 }
