9818c72f6ad9f02057ed597f4d3f248cf1badf62
Carlos Henrique <developerch@outlook.com>
2021-09-17 09:10:00 -0300

    Update VehicleService @Transactional annotation included

--- backend/src/main/java/br/com/movements/wharehouse/services/VehicleService.java
+++ backend/src/main/java/br/com/movements/wharehouse/services/VehicleService.java
@@ -9,6 +9,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.movements.dto.VehicleDTO;
 import br.com.movements.wharehouse.domain.Vehicle;
@@ -21,23 +23,27 @@ public class VehicleService{
 	@Autowired
 	private VehicleRepository repository;
 	
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public Page<VehicleDTO> findAll(Pageable pageable){
 		Page<Vehicle> pageVehicles = repository.findAll(pageable);
 		Page<VehicleDTO> pageVehiclesDTO = pageVehicles.map(obj -> (new VehicleDTO(obj)));
 		return pageVehiclesDTO;
 	}
 	
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public List<VehicleDTO> findAll(){
 		List<Vehicle> listVehicles = repository.findAll();
 		List<VehicleDTO> listVehiclesDTO = listVehicles.stream().map(obj -> (new VehicleDTO(obj))).collect(Collectors.toList());
 		return listVehiclesDTO;
 	}
 	
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public Vehicle find(Long id) {
 		Optional<Vehicle> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + " Tipo: " + Vehicle.class.getName()));
 	}
 	
+	@Transactional
 	public Vehicle create(Vehicle obj) {
 		obj.setId(null);
 		obj.setActive(true);	
@@ -48,17 +54,20 @@ public class VehicleService{
 		return new Vehicle(objDTO.getId(), objDTO.getPlaqueOne(), objDTO.getPlaqueTwo(), objDTO.getActive());
 	}
 	
+	@Transactional
 	public Vehicle update(Vehicle obj) {
 		Vehicle result = find(obj.getId());
-		if(obj.getPlaqueTwo() == null) {
+		System.out.println(obj.getPlaqueTwo().equals(null));
+		if(obj.getPlaqueTwo().equals(null)) {
 			obj.setPlaqueTwo(result.getPlaqueTwo());
 		}
-		if(obj.getActive() == null) {
+		if(obj.getActive().equals(null)) {
 			obj.setActive(result.getActive());
 		}
 		return repository.save(obj);
 	}
 	
+	@Transactional
 	public void delete(Long id) {
 		 find(id);
 		 try {
